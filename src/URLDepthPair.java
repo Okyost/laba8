0dd940d7a861457ab07d12545d1ae082b84f2af6
Okyost <Gr1nya24@yandex.ru>
2020-06-06 12:13:45 +0300

    Выполнил 8 лабораторную работу

--- src/URLDepthPair.java
+++ src/URLDepthPair.java
@@ -1,24 +1,46 @@
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class URLDepthPair {
-    String url;
-    int depth;
 
-    URLDepthPair(int depth,String url){
+    private String host;
+    private int depth;
+    private URL url;
+
+    public URLDepthPair(String host, int depth) {
+        this.host = host;
         this.depth = depth;
-        this.url = url;
-    }
-    URLDepthPair(String url){
-        this.url =url;
-        this.depth = 0;
+        try {
+            url = new URL(host);
+        } catch (MalformedURLException e) {
+            System.err.println("MalformedURLException: " + e.getMessage());
+            return;
+        }
     }
 
-    public int getDepth() {
+    // getter URL-адреса
+    public String get_url() {
+        return host;
+    }
+
+    // getter глубины
+    public int get_depth() {
         return depth;
     }
 
-    public String getUrl() {
-        return url;
+    // getter хоста
+    public String get_host() {
+        return url.getHost();
     }
-    public String toString(){
-        return(url + " "+ depth);
+
+    // Получение пути
+    public String get_path() {
+        return url.getPath();
+    }
+
+    // getter ссылки
+    public String get_link() {
+        return url.getProtocol() + "://" + url.getHost();
     }
 }
